c60cea4389b33a115e52ad6f4d8d8422ec7a9ad6
pavladan <44526604+pavladan@users.noreply.github.com>
2019-03-28 22:40:37 +0300

    empy react-redux-route project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {connect} from 'react-redux'
 import './App.css';
+import {Link} from 'react-router-dom'
 
 class App extends Component {
-  render() {
+  
+  
+  render() {   
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  (state,ownProps) => ({
+    
+  }),
+  dispatch => ({
+    
+  })
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router';
+import {createBrowserHistory} from 'history';
+import {Route,Switch} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const history = createBrowserHistory();
+const store = createStore(
+  reducer(history),
+  composeWithDevTools(applyMiddleware(routerMiddleware(history),thunk))
+);
+ReactDOM.render(
+  <Provider store ={store}>
+     <ConnectedRouter history={history}>
+       <Switch>
+        <Route path="/" exact component={App} />
+       </Switch>
+     </ConnectedRouter>
+   </Provider>,
+   document.getElementById('root')
+);
 serviceWorker.unregister();
