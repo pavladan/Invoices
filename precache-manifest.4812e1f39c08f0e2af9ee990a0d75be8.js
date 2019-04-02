a3b8cf76aa2f89a78045b86a78e5893eefed4985
pavladan <44526604+pavladan@users.noreply.github.com>
2019-04-02 18:53:16 +0300

    Updates

---
+++ precache-manifest.4812e1f39c08f0e2af9ee990a0d75be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc396f02fef288395c5b",
+    "url": "/Invoices/static/css/main.091d0856.chunk.css"
+  },
+  {
+    "revision": "bc396f02fef288395c5b",
+    "url": "/Invoices/static/js/main.f98d715f.chunk.js"
+  },
+  {
+    "revision": "63be1341e06951363ab2",
+    "url": "/Invoices/static/js/runtime~main.524c3985.js"
+  },
+  {
+    "revision": "e5630c5388a7e47cc1ea",
+    "url": "/Invoices/static/css/2.7d56b581.chunk.css"
+  },
+  {
+    "revision": "e5630c5388a7e47cc1ea",
+    "url": "/Invoices/static/js/2.3eb41141.chunk.js"
+  },
+  {
+    "revision": "d729ba84e7f041fe37a8f01b3ae4206f",
+    "url": "/Invoices/index.html"
+  }
+];
